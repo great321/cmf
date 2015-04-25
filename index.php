a2698c9e850e8476855ead9f7396477415708a64
great321 <developermoroz@gmail.com>
2015-04-25 12:18:38 +0300

    22-11 21.04.2015

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * Copyright (C) 2015 user
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 ini_set('display_errors', 1);
 require_once 'mvc/bootstrap.php';
